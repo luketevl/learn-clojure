09663e0da8692bd572e5b27b33796ce3f3b7a4e4
Lucas Henrique <luketevl@gmail.com>
2017-02-14 00:14:22 -0200

    readme project

--- alura/hangman/src/hangman/core.clj
+++ alura/hangman/src/hangman/core.clj
@@ -1,6 +1,26 @@
 (ns hangman.core
   (:gen-class))
 
+(def total-lifes 6)
+
+(defn lose [] (print "You lose"))
+
+(defn game [lifes]
+  (if ( = lifes 0)
+    (lose)
+    (do
+      (print lifes)
+      (game (dec lifes))
+    )
+  )
+)
+
+(def fib [x]
+  (if (= x 0) 0
+  (if (= x 1) 1
+  (+ (fib (dec x)) (fib (- x 2)))))
+)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
