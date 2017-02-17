78ff0a7d99648bba0f80d82c4c651934186c55eb
luketevl <luketevl@gmail.com>
2017-02-17 18:20:55 -0200

    news

--- alura/hangman/src/hangman/core.clj
+++ alura/hangman/src/hangman/core.clj
@@ -4,22 +4,41 @@
 (def total-lifes 6)
 
 (defn lose [] (print "You lose"))
+(defn win [] (print "You win"))
+
+(defn letters-remainder [word hits]
+  (remove (fn [letter] (contains? hits (str letter))) word)
+)
+
+(defn all-words-hits? [words hits]
+  (empty? (letters-remainder words hits))
+)
+
+(defn write-letter! [] (read-line))
+
+(defn try-ok? [letter-try word] (.contains word letter-try))
+
+
+
 
 (defn game [lifes word hits]
-  (if ( = lifes 0)
+  (if (= lifes 0)
     (lose)
-    (if)
-    (do
-      (print lifes)
-      (game (dec lifes))
+    (if (all-words-hits? word hits)
+      (win)
+      (do
+        (check-try (write-letter!) lifes word hits)
+        (print "Try again, bro !")
+      )
+
     )
   )
 )
-
-(def fib [x]
-  (if (= x 0) 0
-  (if (= x 1) 1
-  (+ (fib (dec x)) (fib (- x 2)))))
+(defn check-try [letter-try lifes word hits]
+  (if (try-ok? letter-try word)
+    (game lifes word (conj hits letter-try))
+    (game (dec lifes) word hits)
+  )
 )
 
 (defn -main
